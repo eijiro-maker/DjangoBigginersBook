8565c3e2a88cd2fd805c23bac012629172ff96ee
hayashi eijiro <ejrhys@gmail.com>
2020-07-19 21:14:18 +0900

    blog.view変更

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,13 @@ class PostList(generic.ListView):
     model = Post
     ordering = '-created_at'
 
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category_list']=Category.objects.all()
+        context['tag_list'] = Tag.objects.all()
+        return context
+
+
 class PostDetail(generic.DetailView):
     model = Post
 
